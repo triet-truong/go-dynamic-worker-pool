9b25442dac182bb0ba90b8f031a7db9d45132897
triet-truong-ts <triet.truong@trustingsocial.com>
2023-01-08 14:00:04 +0700

    Init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module dummy-golang.com/v2
+
+go 1.18
+
+require github.com/gocarina/gocsv v0.0.0-20220729221910-a7386ae0b221
+
+require (
+	github.com/braintree/manners v0.0.0-20160418043613-82a8879fc5fd // indirect
+	github.com/gocraft/web v0.0.0-20190207150652-9707327fb69b // indirect
+	github.com/gocraft/work v0.5.1 // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func main() {
+	tasks := make(chan string)
+	quit := make(chan struct{})
+	currentWorkerSize := 3
+	wg := &sync.WaitGroup{}
+	workerSizeConfigFileName := "concurrency.txt"
+
+	go func() {
+		// gracefully shutdown
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		os.Remove(workerSizeConfigFileName)
+		close(tasks)
+	}()
+
+	go updateWorkerSizeByConfigFile(workerSizeConfigFileName, wg, currentWorkerSize, tasks, quit)
+	go produceTasks(tasks)
+	consumeTasks(wg, currentWorkerSize, tasks, quit)
+	wg.Wait()
+}
+
+func worker(wg *sync.WaitGroup, i int, tasks <-chan string, quit <-chan struct{}) {
+	fmt.Println("Start worker ", i)
+	defer wg.Done()
+	for {
+		select {
+		case task, ok := <-tasks:
+			if !ok {
+				return
+			}
+			fmt.Printf("Worker %v, task: %v\n", i, task)
+		case <-quit:
+			fmt.Printf("quit worker %v\n", i)
+			return
+		}
+	}
+
+}
+
+func consumeTasks(wg *sync.WaitGroup, currentWorkerSize int, tasks chan string, quit chan struct{}) {
+	wg.Add(currentWorkerSize)
+	for i := 1; i <= currentWorkerSize; i++ {
+		go worker(wg, i, tasks, quit)
+	}
+}
+
+func produceTasks(tasks chan string) {
+	for i := 0; i <= 100; i++ {
+		tasks <- fmt.Sprintf("task_%v", i)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	fmt.Println("close tasks")
+	close(tasks)
+}
+
+func updateWorkerSizeByConfigFile(fileName string, wg *sync.WaitGroup, currentWorkerSize int, tasks chan string, quit chan struct{}) {
+	for {
+		time.Sleep(1 * time.Second)
+		newWorkerSize, err := readWorkerSizeFromFile(fileName)
+		if err != nil {
+			fmt.Println(err)
+
+			// Write current worker size to file
+			if err := writeWorkerSizeToFile(fileName, fmt.Sprint(currentWorkerSize)); err != nil {
+				fmt.Println(err)
+				return
+			}
+			newWorkerSize = currentWorkerSize
+		}
+
+		if newWorkerSize == currentWorkerSize {
+			continue
+		} else if additionalSize := newWorkerSize - currentWorkerSize; additionalSize > 0 {
+			fmt.Println("newWorkerSize:", newWorkerSize)
+			wg.Add(additionalSize)
+			for i := 0; i < additionalSize; i++ {
+				go worker(wg, 100+i, tasks, quit)
+			}
+		} else {
+			fmt.Println("newWorkerSize:", newWorkerSize)
+			for i := 0; i < -additionalSize; i++ {
+				quit <- struct{}{}
+			}
+		}
+		currentWorkerSize = newWorkerSize
+	}
+}
+
+func readWorkerSizeFromFile(fileName string) (newWorkerSize int, err error) {
+	f, err := os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+	b := make([]byte, 1) // read only one byte
+	f.Read(b)
+	workerSizeStr := string(b)
+	if workerSizeStr == "" {
+		return 0, errors.New("empty WORKER_SIZE")
+	}
+
+	// fmt.Print("workerSizeStr:", workerSizeStr)
+	size, err := strconv.Atoi(workerSizeStr)
+	if err != nil {
+		fmt.Println(err)
+		return size, err
+	}
+	newWorkerSize = size
+	// fmt.Println(" newWorkerSize: ", newWorkerSize)
+	return
+}
+
+func writeWorkerSizeToFile(fileName, numConcurrency string) (err error) {
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return
+	}
+	_, err = f.WriteString(fmt.Sprint(numConcurrency))
+	return err
+}
